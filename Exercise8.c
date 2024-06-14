4137a557d0502dcfcb2895897ad4869304398806
Nurettin-Kaplan <nurettinkaplaan@gmail.com>
2024-06-14 20:02:27 +0300

    Eighth exercise is completed.

---
+++ Exercise8.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+// Print the following table on the screen using the C language.
+//
+//		No		Name 		Surname
+//		1		Nurettin	Kaplan
+//		20		Mustafa		Kemal
+//		335		Cengiz 		Han
+int main(void){
+	
+	printf("No\tName\t\tSurname\n1\tNurettin\tKaplan\n20\tMustafa Kemal\tAtaturk\n335\tCengiz\t\tHan\n");
+	
+	system("pause");
+	return 0;
+}
