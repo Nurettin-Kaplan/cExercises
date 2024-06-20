e45a8275f929a390d80f323e4d985368129da858
Nurettin-Kaplan <nurettinkaplaan@gmail.com>
2024-06-20 17:11:07 +0300

    In exercise thirty-five, the blanks in the code were deleted.

--- Exercise35.c
+++ Exercise35.c
@@ -9,26 +9,16 @@ void PrintNumbers(int num){
 		PrintNumbers(num / 10);
 		int digit = num % 10;
 		switch(digit){
-			case 0:
-				printf("Zero "); break;
-			case 1:
-				printf("One "); break;
-			case 2:
-				printf("Two "); break;
-			case 3:
-				printf("Three "); break;
-			case 4:
-				printf("Four "); break;
-			case 5:
-				printf("Five "); break;
-			case 6:
-				printf("Six "); break;
-			case 7:
-				printf("Seven "); break;
-			case 8:
-				printf("Eight "); break;
-			case 9:
-				printf("Nine "); break;
+			case 0: printf("Zero "); break;
+			case 1: printf("One "); break;
+			case 2: printf("Two "); break;
+			case 3: printf("Three "); break;
+			case 4: printf("Four "); break;
+			case 5: printf("Five "); break;
+			case 6: printf("Six "); break;
+			case 7: printf("Seven "); break;
+			case 8: printf("Eight "); break;
+			case 9:printf("Nine "); break;
 		}
 	}
 }
